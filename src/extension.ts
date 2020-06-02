8bcad2da4bb2b22a06680924642f6a6d5316b491
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-06-01 20:02:15 -0700

    basic edit/select mode toggle

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+enum InputMode {
+	Edit = 'edit',
+	Select = 'select'
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "leap" is now active!');
+	context.workspaceState.update('leapMode', InputMode.Edit);
+	const statusBarItem = vscode.window.createStatusBarItem()
+	statusBarItem.text = InputMode.Edit;
+	statusBarItem.show();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('leap.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const toggleCommand = vscode.commands.registerCommand('leap.toggleMode', () => {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from leap!');
+		const currentMode = context.workspaceState.get<InputMode>('leapMode');
+		const newMode = (currentMode === InputMode.Edit) ? InputMode.Select : InputMode.Edit;
+		vscode.commands.executeCommand('setContext', 'leap.mode', newMode)
+		context.workspaceState.update('leapMode', newMode);
+		statusBarItem.text = newMode;
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(toggleCommand);
+	context.subscriptions.push(statusBarItem);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
