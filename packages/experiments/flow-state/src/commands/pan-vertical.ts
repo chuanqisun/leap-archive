b086e68011ecdebf650dc6441884803e1ba854f5
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-06-07 21:49:25 -0700

    fix typos

--- packages/experiments/flow-state/src/commands/pan-vertical.ts
+++ packages/experiments/flow-state/src/commands/pan-vertical.ts
@@ -1,24 +1,23 @@
 import * as vscode from "vscode";
-import { cursorActivePosition, cursorAnchorPosition, cursorPositionMode, saveCursorActivePosition, saveSelection } from "../utils/cursor-memory";
+import { cursorPositionMode, savedSelection, saveSelection } from "../utils/cursor-memory";
 import { CursorPositionMode } from "../utils/cursor-position";
 
 export function activatePanVerticalCommands(context: vscode.ExtensionContext) {
   const panUpCommand = vscode.commands.registerTextEditorCommand("flowState.panUp", (editor) => {
-    const candidateLine: number = cursorActivePosition.line - 1;
+    const candidateLine: number = savedSelection.active.line - 1;
 
     if (candidateLine < 0) {
       console.log("[pan-v] document top, noop");
       return;
     }
 
-    const idealActive = new vscode.Position(candidateLine, cursorActivePosition.character);
-    const idealAnchor = new vscode.Position(candidateLine, cursorAnchorPosition.character);
+    const idealActive = new vscode.Position(candidateLine, savedSelection.active.character);
+    const idealAnchor = new vscode.Position(candidateLine, savedSelection.anchor.character);
 
     // will by on empty line. Since home/start mode will be ambigous, we will manually update position to avoid auto saving mode
     if (editor.document.lineAt(candidateLine).isEmptyOrWhitespace) {
       console.log("[pan-v] will be on empty line, use cursor up");
 
-      saveCursorActivePosition(candidateLine, cursorActivePosition.character);
       saveSelection(new vscode.Selection(idealAnchor, idealActive));
       vscode.commands.executeCommand("cursorUp");
 
@@ -43,21 +42,20 @@ export function activatePanVerticalCommands(context: vscode.ExtensionContext) {
   const panDownCommand = vscode.commands.registerTextEditorCommand("flowState.panDown", (editor) => {
     const document = editor.document;
     const max = document.lineCount - 1;
-    const candidateLine: number = cursorActivePosition.line + 1;
+    const candidateLine: number = savedSelection.active.line + 1;
 
     if (candidateLine > max) {
       console.log("[pan-v] document bottom, noop");
       return;
     }
 
-    const idealActive = new vscode.Position(candidateLine, cursorActivePosition.character);
-    const idealAnchor = new vscode.Position(candidateLine, cursorAnchorPosition.character);
+    const idealActive = new vscode.Position(candidateLine, savedSelection.active.character);
+    const idealAnchor = new vscode.Position(candidateLine, savedSelection.anchor.character);
 
     // will by on empty line. Since home/start mode will be ambigous, we will manually update position to avoid auto saving mode
     if (editor.document.lineAt(candidateLine).isEmptyOrWhitespace) {
       console.log("[pan-v] will be on empty line, use cursor down");
 
-      saveCursorActivePosition(candidateLine, cursorActivePosition.character);
       saveSelection(new vscode.Selection(idealAnchor, idealActive));
       vscode.commands.executeCommand("cursorDown");
 
@@ -96,7 +94,6 @@ function selectNearestWordOrPositionOnLine(editor: vscode.TextEditor, idealAncho
   if (candidateWordRange) {
     editor.selection = new vscode.Selection(candidateWordRange.start, candidateWordRange.end);
     editor.revealRange(candidateWordRange);
-    saveCursorActivePosition(idealActive.line, idealActive.character);
-    saveSelection(new vscode.Selection(idealAnchor, idealAnchor));
+    saveSelection(new vscode.Selection(idealAnchor, idealActive));
   }
 }
--- packages/experiments/flow-state/src/utils/cursor-memory.ts
+++ packages/experiments/flow-state/src/utils/cursor-memory.ts
@@ -1,8 +1,6 @@
 import * as vscode from "vscode";
 import { CursorPositionMode, getCurrentCursorPositionMode } from "./cursor-position";
 
-export let cursorActivePosition: vscode.Position;
-export let cursorAnchorPosition: vscode.Position;
 export let cursorPositionMode: CursorPositionMode;
 
 export let savedSelection: vscode.Selection;
@@ -13,14 +11,12 @@ export function handleCusorPositionChange(changeEvent: vscode.TextEditorSelectio
   if (!skipNextAutoPositionSaveByCommand) {
     const editor = changeEvent.textEditor;
 
-    cursorActivePosition = new vscode.Position(editor.selection.active.line, editor.selection.active.character);
-    cursorAnchorPosition = new vscode.Position(editor.selection.anchor.line, editor.selection.anchor.character);
     savedSelection = new vscode.Selection(editor.selection.anchor, editor.selection.active);
 
     cursorPositionMode = getCurrentCursorPositionMode(editor);
 
     console.log(
-      `[cursor mem] auto save: ${cursorAnchorPosition.line}:${cursorAnchorPosition.character}->${cursorActivePosition.line}:${cursorActivePosition.character}|${cursorPositionMode}`
+      `[cursor mem] auto save: ${savedSelection.anchor.line}:${savedSelection.anchor.character}->${savedSelection.active.line}:${savedSelection.active.character}|${cursorPositionMode}`
     );
   }
 
@@ -30,21 +26,9 @@ export function handleCusorPositionChange(changeEvent: vscode.TextEditorSelectio
   }
 }
 
-export function saveCursorActivePosition(line: number, character: number, newCursorPositionMode?: CursorPositionMode, skipNextAutoSave = true) {
-  // assuming the caller of this function is in a command
-  skipNextAutoSave && skipNextAutoSaveCausedByCommand();
-  cursorActivePosition = new vscode.Position(line, character);
-
-  if (newCursorPositionMode) {
-    cursorPositionMode = newCursorPositionMode;
-  }
-
-  console.log(`[cursor mem] manual save: ${[line, character]}`);
-}
-
 export function saveSelection(selection: vscode.Selection, newCursorPositionMode?: CursorPositionMode, skipNextAutoSave = true) {
   skipNextAutoSave && skipNextAutoSaveCausedByCommand();
-  savedSelection = new vscode.Selection(selection.anchor, selection.anchor);
+  savedSelection = new vscode.Selection(selection.anchor, selection.active);
 
   if (newCursorPositionMode) {
     cursorPositionMode = newCursorPositionMode;
