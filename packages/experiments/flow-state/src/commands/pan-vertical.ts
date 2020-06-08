a4a0c6566becd9b7add87c1f33a2b14277231e38
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-06-07 21:35:19 -0700

    refactor: save both active and anchor

--- packages/experiments/flow-state/src/commands/pan-vertical.ts
+++ packages/experiments/flow-state/src/commands/pan-vertical.ts
@@ -11,15 +11,15 @@ export function activatePanVerticalCommands(context: vscode.ExtensionContext) {
       return;
     }
 
+    const idealActive = new vscode.Position(candidateLine, cursorActivePosition.character);
+    const idealAnchor = new vscode.Position(candidateLine, cursorAnchorPosition.character);
+
     // will by on empty line. Since home/start mode will be ambigous, we will manually update position to avoid auto saving mode
     if (editor.document.lineAt(candidateLine).isEmptyOrWhitespace) {
       console.log("[pan-v] will be on empty line, use cursor up");
 
-      const newActive = new vscode.Position(candidateLine, cursorActivePosition.character);
-      const newAnchor = new vscode.Position(candidateLine, cursorAnchorPosition.character);
-
       saveCursorActivePosition(candidateLine, cursorActivePosition.character);
-      saveSelection(new vscode.Selection(newAnchor, newActive));
+      saveSelection(new vscode.Selection(idealAnchor, idealActive));
       vscode.commands.executeCommand("cursorUp");
 
       return;
@@ -37,7 +37,7 @@ export function activatePanVerticalCommands(context: vscode.ExtensionContext) {
 
     console.log("[pan-v] line middle, select nearest word");
 
-    selectNearestWordOrPositionOnLine(editor, candidateLine, cursorActivePosition.character);
+    selectNearestWordOrPositionOnLine(editor, idealAnchor, idealActive);
   });
 
   const panDownCommand = vscode.commands.registerTextEditorCommand("flowState.panDown", (editor) => {
@@ -50,15 +50,15 @@ export function activatePanVerticalCommands(context: vscode.ExtensionContext) {
       return;
     }
 
+    const idealActive = new vscode.Position(candidateLine, cursorActivePosition.character);
+    const idealAnchor = new vscode.Position(candidateLine, cursorAnchorPosition.character);
+
     // will by on empty line. Since home/start mode will be ambigous, we will manually update position to avoid auto saving mode
     if (editor.document.lineAt(candidateLine).isEmptyOrWhitespace) {
       console.log("[pan-v] will be on empty line, use cursor down");
 
-      const newActive = new vscode.Position(candidateLine, cursorActivePosition.character);
-      const newAnchor = new vscode.Position(candidateLine, cursorAnchorPosition.character);
-
       saveCursorActivePosition(candidateLine, cursorActivePosition.character);
-      saveSelection(new vscode.Selection(newAnchor, newActive));
+      saveSelection(new vscode.Selection(idealAnchor, idealActive));
       vscode.commands.executeCommand("cursorDown");
 
       return;
@@ -75,7 +75,7 @@ export function activatePanVerticalCommands(context: vscode.ExtensionContext) {
 
     console.log("[pan-v] line middle, select nearest word");
 
-    selectNearestWordOrPositionOnLine(editor, candidateLine, cursorActivePosition.character);
+    selectNearestWordOrPositionOnLine(editor, idealAnchor, idealActive);
   });
 
   context.subscriptions.push(panUpCommand);
@@ -83,13 +83,12 @@ export function activatePanVerticalCommands(context: vscode.ExtensionContext) {
 }
 
 // TODO: support the same select regex as horizontal movement
-function selectNearestWordOrPositionOnLine(editor: vscode.TextEditor, line: number, character: number) {
+function selectNearestWordOrPositionOnLine(editor: vscode.TextEditor, idealAnchor: vscode.Position, idealActive: vscode.Position) {
   let candidateWordRange: vscode.Range | undefined;
 
-  const referencePosition = new vscode.Position(line, character);
-  const safePosition = editor.document.validatePosition(referencePosition);
+  const safePosition = idealActive;
 
-  candidateWordRange = editor.document.getWordRangeAtPosition(new vscode.Position(line, safePosition.character));
+  candidateWordRange = editor.document.getWordRangeAtPosition(new vscode.Position(safePosition.line, safePosition.character));
   if (!candidateWordRange) {
     candidateWordRange = new vscode.Range(safePosition, safePosition);
   }
@@ -97,6 +96,7 @@ function selectNearestWordOrPositionOnLine(editor: vscode.TextEditor, line: numb
   if (candidateWordRange) {
     editor.selection = new vscode.Selection(candidateWordRange.start, candidateWordRange.end);
     editor.revealRange(candidateWordRange);
-    saveCursorActivePosition(line, character);
+    saveCursorActivePosition(idealActive.line, idealActive.character);
+    saveSelection(new vscode.Selection(idealAnchor, idealAnchor));
   }
 }
